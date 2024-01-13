97bfe7c2e731f429f2f1bbf8c3a59f000ab22416
Sungwoo Lee <swlee052@gmail.com>
2024-01-12 23:28:35 -0500

    index file update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 export {
   IAuthPayload,
-  IAuth,
   IAuthDocument,
   IAuthBuyerMessageDetails,
   IEmailMessageDetails,
+  IAuth,
   ISignUpPayload,
   ISignInPayload,
   IForgotPassword,
@@ -14,9 +14,63 @@ export {
   IAuthResponse,
   IAuthUser,
 } from './auth.interface';
-
+export { IBuyerDocument, IReduxBuyer } from './buyer.interface';
+export {
+  IConversationDocument,
+  IMessageDocument,
+  IMessageDetails,
+  IChatBoxProps,
+  IChatSellerProps,
+  IChatBuyerProps,
+  IChatMessageProps,
+} from './chat.interface';
+export {
+  GigType,
+  ICreateGig,
+  ISellerGig,
+  IGigContext,
+  IGigsProps,
+  IGigCardItems,
+  ISelectedBudget,
+  IGigViewReviewsProps,
+  IGigInfo,
+  IGigTopProps,
+} from './gig.interface';
+export {
+  IOrderNotifcation,
+  IOffer,
+  IExtendedDelivery,
+  IDeliveredWork,
+  IOrderEvents,
+  IOrderReview,
+  IOrderMessage,
+  IOrderDocument,
+} from './order.interface';
+export {
+  IReviewMessageDetails,
+  IRatingTypes,
+  IRatingCategories,
+  IRatingCategoryItem,
+  IReviewDocument,
+} from './review.interface';
+export {
+  ISearchResult,
+  IHitsTotal,
+  IQueryList,
+  IQueryString,
+  ITerm,
+  IPaginateProps,
+} from './search.interface';
+export {
+  SellerType,
+  ILanguage,
+  IExperience,
+  IEducation,
+  ICertificate,
+  ISellerDocument,
+} from './seller.interface';
+export { IEmailLocals } from './email.interface';
 export { uploads, videoUpload } from './cloudinary-upload';
-
 export {
   IErrorResponse,
   IError,
@@ -26,9 +80,8 @@ export {
   NotAuthorizedError,
   FileTooLargeError,
   ServerError,
-  ErrnoException,
+  ErrnoException
 } from './error-handler';
-
 export { verifyGatewayRequest } from './gateway-middleware';
 export { winstonLogger } from './logger';
 export {
@@ -36,5 +89,5 @@ export {
   lowerCase,
   toUpperCase,
   isEmail,
-  isDataURL,
+  isDataURL
 } from './helpers';
